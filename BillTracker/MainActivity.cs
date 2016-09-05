12287c226ceeff0931ef34e1f7f465d06a2eb408
DJ Rausch <djrausch3@gmail.com>
2016-09-05 14:05:30 -0700

    * .gitignore: Initial Commit
    * BillTracker.sln:
    * packages.config:
    * MainActivity.cs:
    * BillTracker.csproj:
    * AboutAssets.txt:
    * AddOrEditBillActivity.cs:
    * Main.axml:
    * AssemblyInfo.cs:
    * Colors.xml:
    * Styles.xml:
    * Dimens.xml:
    * Strings.xml:
    * ids.xml:
    * AboutResources.txt:
    * Icon.png:
    * Resource.designer.cs:
    * AndroidManifest.xml:
    * Icon.png:
    * Icon.png:
    * dimens.xml:
    * colors.xml:
    * styles.xml:
    * menu_main.xml:
    * strings.xml:
    * Icon.png:
    * row_paid.xml:
    * row_bill.xml:
    * MainContent.axml:
    * Icon.png:
    * pref_headers.xml:
    * pref_general.xml:
    * backup_schema.xml:
    * styles.xml:
    * pref_data_sync.xml:
    * add_bill_menu.xml:
    * content_main.xml:
    * view_bill_menu.xml:
    * edit_bill_menu.xml:
    * dimens.xml:
    * activity_main.xml:
    * pref_notification.xml:
    * ic_launcher.png:
    * ic_launcher.png:
    * activity_sign_in.xml:
    * ic_launcher.png:
    * activity_add_bill.xml:
    * ic_notification.png:
    * ic_notification.png:
    * ic_launcher.png:
    * ic_launcher.png:
    * ic_notification.png:
    * ic_add_white_24dp.png:
    * ic_notification.png:
    * ic_add_white_24dp.png:
    * ic_sync_black_24dp.xml:
    * ic_info_black_24dp.xml:
    * ic_info_black_24dp.png:
    * ic_notification.png:
    * ic_add_white_24dp.png:
    * activity_google_login.xml:
    * ic_sync_black_24dp.png:
    * ic_sync_black_24dp.png:
    * ic_info_black_24dp.png:
    * ic_notification.png:
    * ic_notification.png:
    * ic_clear_white_24dp.png:
    * ic_info_black_24dp.png:
    * ic_sync_black_24dp.png:
    * ic_add_white_24dp.png:
    * ic_notification.png:
    * ic_notification.png:
    * ic_clear_white_24dp.png:
    * ic_info_black_24dp.png:
    * ic_notification.png:
    * ic_notification.png:
    * ic_clear_white_24dp.png:
    * ic_add_white_24dp.png:
    * ic_sync_black_24dp.png:
    * ic_notification.png:
    * ic_info_black_24dp.xml:
    * ic_add_white_24dp.png:
    * ic_sync_black_24dp.xml:
    * ic_add_white_24dp.png:
    * ic_sync_black_24dp.png:
    * ic_info_black_24dp.png:
    * ic_clear_white_24dp.png:
    * ic_notification.png:
    * activity_add_or_edit_bill.xml:
    * ic_info_black_24dp.png:
    * ic_sync_black_24dp.png:
    * ic_add_white_24dp.png:
    * ic_sync_black_24dp.png:
    * ic_clear_white_24dp.png:
    * ic_notification.png:
    * ic_info_black_24dp.png:
    * ic_notification.png:
    * ic_clear_white_24dp.png:
    * ic_notification.png:
    * ic_add_white_24dp.png:
    * ic_notification.png:
    * ic_clear_white_24dp.png:
    * ic_sync_black_24dp.png:
    * ic_info_black_24dp.png:
    * activity_view_bill_details.xml:
    * ic_clear_white_24dp.png:
    * ic_add_white_24dp.png:
    * ic_notification.png:
    * ic_info_black_24dp.png:
    * ic_sync_black_24dp.png:
    * ic_info_black_24dp.png:
    * ic_notification.png:
    * ic_sync_black_24dp.png:
    * ic_clear_white_24dp.png:
    * ic_notifications_black_24dp.xml:
    * ic_notification.png:
    * ic_clear_white_24dp.png:
    * ic_notifications_black_24dp.png:
    * ic_notifications_black_24dp.png:
    * ic_notifications_black_24dp.png:
    * ic_notifications_black_24dp.png:
    * ic_notifications_black_24dp.png:
    * ic_notifications_black_24dp.xml:
    * ic_notifications_black_24dp.png:
    * ic_notifications_black_24dp.png:
    * ic_notifications_black_24dp.png:
    * ic_notifications_black_24dp.png:
    * ic_notifications_black_24dp.png:

---
+++ BillTracker/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+
+namespace BillTracker
+{
+	[Activity(Label = "Bill Tracker", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/AppTheme.NoActionBar")]
+	public class MainActivity : AppCompatActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.Main);
+
+			Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.main_toolbar);
+			SetSupportActionBar(toolbar);
+		}
+	}
+}
+
+
